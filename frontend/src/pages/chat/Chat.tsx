9e7fdadfd89310a5f3091698a29fe9ae96858977
Ganesh, Srikanth <Srikanth.Ganesh@Dell.com>
2024-03-06 15:37:28 +0530

    changing tags and default titles

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -766,7 +766,7 @@ const Chat = () => {
                             </Stack>
                             <QuestionInput
                                 clearOnSend
-                                placeholder="Type a new question..."
+                                placeholder="Enter the new requirement details here..."
                                 disabled={isLoading}
                                 onSend={(question, id) => {
                                     appStateContext?.state.isCosmosDBAvailable?.cosmosDB ? makeApiRequestWithCosmosDB(question, id) : makeApiRequestWithoutCosmosDB(question, id)
